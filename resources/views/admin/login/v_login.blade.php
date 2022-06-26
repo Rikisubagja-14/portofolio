e4b51593be2ae2fe96dee31499b87359e332d325
Rikisubagja14 <riki43733@gmail.com>
2022-06-27 02:03:00 +0700

    update

--- resources/views/admin/login/v_login.blade.php
+++ resources/views/admin/login/v_login.blade.php
@@ -7,8 +7,8 @@
 
     <title>Log in</title>
 
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link
+        rel="stylesheet"href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('template/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('template/dist/css/adminlte.min.css?v=3.2.0') }}">
