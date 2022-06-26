b2656946b666f5878addfdc22aa833ac22bd656d
Rikisubagja14 <riki43733@gmail.com>
2022-06-26 20:19:51 +0700

    update

--- resources/views/admin/login/v_login.blade.php
+++ resources/views/admin/login/v_login.blade.php
@@ -8,9 +8,9 @@
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('template/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('template/dist/css/adminlte.min.css?v=3.2.0') }}">
+    <link rel="stylesheet" href="{{ !!asset('template/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('template/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('template/dist/css/adminlte.min.css?v=3.2.0') }}">
 </head>
 
 <body class="hold-transition login-page">
@@ -20,7 +20,6 @@
             background-image: url('tempassetlanding/assets/img/riki.jpg');
             background-size: 100% !important;
         }
-
     </style>
     <div class="login-box">
         <div class="card card-outline card-primary">
@@ -96,11 +95,11 @@
     </div>
 
 
-    <script src="{{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ !!asset('template/plugins/jquery/jquery.min.js') }}"></script>
 
-    <script src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ !!asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-    <script src="{{ asset('template/dist/js/adminlte.min.js?v=3.2.0') }}"></script>
+    <script src="{{ !!asset('template/dist/js/adminlte.min.js?v=3.2.0') }}"></script>
 </body>
 
 </html>
--- resources/views/landingpage/layout/index.blade.php
+++ resources/views/landingpage/layout/index.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Favicons -->
     {{-- <link rel="icon" href="{{asset('tempassetlanding/assets/img/favicon.png')}}" > --}}
-    <link rel="apple-touch-icon" href="{{asset('tempassetlanding/assets/img/apple-touch-icon.png')}}" >
+    <link rel="apple-touch-icon" href="{{ !!asset('tempassetlanding/assets/img/apple-touch-icon.png') }}">
 
     <!-- Google Fonts -->
     <link
@@ -19,15 +19,16 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link rel="stylesheet" href="{{ asset('tempassetlanding/assets/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('tempassetlanding/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('tempassetlanding/assets/vendor/boxicons/css/boxicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('tempassetlanding/assets/vendor/glightbox/css/glightbox.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('tempassetlanding/assets/vendor/remixicon/remixicon.css') }}">
-    <link rel="stylesheet" href="{{ asset('tempassetlanding/assets/vendor/swiper/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('tempassetlanding/assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ !!asset('tempassetlanding/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('tempassetlanding/assets/vendor/boxicons/css/boxicons.min.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('tempassetlanding/assets/vendor/glightbox/css/glightbox.min.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('tempassetlanding/assets/vendor/remixicon/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('tempassetlanding/assets/vendor/swiper/swiper-bundle.min.css') }}">
 
     <!-- Template Main CSS File -->
-    <link rel="stylesheet" href="{{ asset('tempassetlanding/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ !!asset('tempassetlanding/assets/css/style.css') }}">
 </head>
 
 <body>
@@ -43,13 +44,13 @@
 
         <!-- ======= About Me ======= -->
         <div class="about-me container">
-           @include('landingpage.L_about.index')
+            @include('landingpage.L_about.index')
         </div>
         <!-- End About Me -->
 
         <!-- ======= Skills  ======= -->
         <div class="skills container">
-        @include('landingpage.L_skills.index')
+            @include('landingpage.L_skills.index')
         </div>
         <!-- End Skills -->
 
@@ -64,21 +65,21 @@
 
     <!-- ======= Resume Section ======= -->
     <section id="resume" class="resume">
-      <div class="container">
-       @include('landingpage.L_resume.index')
-      </div>
+        <div class="container">
+            @include('landingpage.L_resume.index')
+        </div>
     </section>
     <!-- End Resume Section -->
 
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
-       @include('landingpage.L_porto.index')
+        @include('landingpage.L_porto.index')
     </section>
     <!-- End Portfolio Section -->
 
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
-       @include('landingpage.L_contact.index')
+        @include('landingpage.L_contact.index')
     </section>
     <!-- End Contact Section -->
     <!-- /.content-wrapper -->
@@ -88,19 +89,19 @@
     </footer>
 
 
- 
+
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('tempassetlanding/assets/vendor/purecounter/purecounter.js') }}"></script>
-    <script src="{{ asset('tempassetlanding/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('tempassetlanding/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('tempassetlanding/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-    <script src="{{ asset('tempassetlanding/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('tempassetlanding/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
-    <script src="{{ asset('tempassetlanding/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/vendor/purecounter/purecounter.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/vendor/php-email-form/validate.js') }}"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{ asset('tempassetlanding/assets/js/main.js') }}"></script>
+    <script src="{{ !!asset('tempassetlanding/assets/js/main.js') }}"></script>
 
 </body>
 
